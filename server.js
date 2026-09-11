68a022667c444a49ca3837f6079b4c9ce33fb403
J Venugopal Naidu <jvenugopal23@gmail.com>
2026-09-11 16:01:25 +0530

    Prepare server for Render

--- server.js
+++ server.js
@@ -238,6 +238,8 @@ app.get("/receipt/:id", async (req, res) => {
 });
 
 // Start Server
-app.listen(3000, () => {
-    console.log("Server Running on Port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server Running on Port ${PORT}`);
 });
