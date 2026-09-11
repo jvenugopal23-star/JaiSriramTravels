dd6ad9c790952dd5d4b125cca4a7fe00cd39d468
J Venugopal Naidu <jvenugopal23@gmail.com>
2026-09-11 20:54:51 +0530

    Fix Gmail SMTP for deployment

--- server.js
+++ server.js
@@ -6,14 +6,12 @@ const Review = require("./models/Review")
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-
     tls: {
         rejectUnauthorized: false
     }
